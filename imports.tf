be313fd35c37f45fa1969026d2fee3a448fca06b
hakutyo0828 <masamune615msmn@gmail.com>
2025-07-06 17:07:31 +0900

    nat-gw.tf

---
+++ imports.tf
@@ -0,0 +1,16 @@
+#EC2用インポートブロック
+# import {
+#   to = aws_instance.ec2_bastion
+#   id = "(instance-id)"
+# }
+
+#NAT Gateway用インポートブロック
+# import {
+#   to = aws_nat_gateway.nat_gw01
+#   id = "nat-05c9eacf34fd35d36"
+# }
+
+# import {
+#   to = aws_nat_gateway.nat_gw02
+#   id = "nat-03677d6f9b3bcd4a5"
+# }
